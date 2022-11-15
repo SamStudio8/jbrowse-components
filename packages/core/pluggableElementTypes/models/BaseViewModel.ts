3b6add377c4afef16740a2b7a8c3d987abcabef1
Colin <colin.diesh@gmail.com>
2022-11-15 09:25:06 -0700

    Put minimize in track menu

--- packages/core/pluggableElementTypes/models/BaseViewModel.ts
+++ packages/core/pluggableElementTypes/models/BaseViewModel.ts
@@ -2,27 +2,53 @@ import { types, Instance } from 'mobx-state-tree'
 import { ElementId } from '../../util/types/mst'
 import { MenuItem } from '../../ui'
 
+/**
+ * #stateModel BaseViewModel
+ */
 const BaseViewModel = types
   .model('BaseView', {
+    /**
+     * #property
+     */
     id: ElementId,
+    /**
+     * #property
+     */
     displayName: types.maybe(types.string),
+    /**
+     * #property
+     */
     minimized: false,
   })
   .volatile(() => ({
     width: 800,
   }))
   .views(() => ({
+    /**
+     * #getter
+     */
     menuItems(): MenuItem[] {
       return []
     },
   }))
   .actions(self => ({
+    /**
+     * #action
+     */
     setDisplayName(name: string) {
       self.displayName = name
     },
+    /**
+     * #action
+     * setWidth is updated by a ResizeObserver generally, the views often need
+     * to know how wide they are to properly draw genomic regions
+     */
     setWidth(newWidth: number) {
       self.width = newWidth
     },
+    /**
+     * #action
+     */
     setMinimized(flag: boolean) {
       self.minimized = flag
     },
